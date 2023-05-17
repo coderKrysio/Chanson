09f211601bae0296619ae2d9b3aeb6e9f2868044
Anushree <anushreejaiswal.srn@gmail.com>
2023-05-17 22:50:46 +0530

    added components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,39 @@
+import MusicPlayer from '@/components/MusicPlayer';
+import Sidebar from '@/components/Sidebar';
+import TopPlay from '@/components/TopPlay';
+import SearchBar from '@/components/SearchBar';
+// import { useSelector } from "react-redux";
+
 export default function Home() {
+  // const { activeSong } = useSelector((state: any) => state.player);
+
   return (
-    <div>
-      hello
+    <div className="relative flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <SearchBar />
+
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+          {/* <Route path="/" element={<Discover />} />
+          <Route path="/top-artists" element={<TopArtists />} />
+          <Route path="/top-charts" element={<TopCharts />} />
+          <Route path="/around-you" element={<AroundYou />} />
+          <Route path="/artists/:id" element={<ArtistDetails />} />
+          <Route path="/songs/:songid" element={<SongDetails />} />
+          <Route path="/search/:searchTerm" element={<Search />} /> */}
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">
+            <TopPlay />
+          </div>
+        </div>
+      </div>
+
+    {/* {activeSong?.title && (
+      <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+        <MusicPlayer />
+      </div>
+    )} */}
     </div>
-  )
+  );
 }
